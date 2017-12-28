3ead01055d7902283ae6557fdfc3545f1bb07ccb
M Ivaniushchenko <666tos@gmail.com>
2017-12-28 09:10:00 +0100

    added presenting normal and buggy view controllers

--- MaterialSafariBug/ViewController.swift
+++ MaterialSafariBug/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction private func openSafariPressed() {
+    @IBAction private func openBuggySafariPressed() {
         guard let url = URL(string: "https://google.com") else {
             return
         }
@@ -30,6 +30,43 @@ class ViewController: UIViewController {
         
         let navigationController = NavigationController(rootViewController: safariViewController)
         self.present(navigationController, animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction private func openSafariPressed() {
+        guard let url = URL(string: "https://google.com") else {
+            return
+        }
+        
+        let safariViewController = SFSafariViewController(url: url)
+        
+        let navigationController = CustomNavigationController(rootViewController: safariViewController)
+        self.present(navigationController, animated: true)
     }
 }
 
+class CustomNavigationController: NavigationController {
+    override func prepare() {
+        
+        // Do the same as NavigationController does, but not calling 'isMotionEnabled = true'.
+        // Enabling motion causes SFSafariViewController to not loading content
+        
+//        isMotionEnabled = true
+        view.clipsToBounds = true
+        view.backgroundColor = .white
+        view.contentScaleFactor = Screen.scale
+        
+        let navigationBar = self.navigationBar as! NavigationBar
+        navigationBar.frame.size.width = view.bounds.width
+        navigationBar.heightPreset = .normal
+        
+        // This ensures the panning gesture is available when going back between views.
+        if let v = interactivePopGestureRecognizer {
+            v.isEnabled = true
+            v.delegate = self
+        }
+    }
+}
